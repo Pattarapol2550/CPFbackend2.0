0fe01e1f6952ca1723bdaa522e8977ef5f44ec44
Pattarapol Jamsai <6833212121@student.chula.ac.th>
2026-06-22 14:32:49 +0700

    allow method

--- app/main.py
+++ app/main.py
@@ -39,8 +39,7 @@ def create_app() -> FastAPI:
         CORSMiddleware,
         allow_origins=CORS_ORIGINS,
         allow_credentials=True,
-        allow_methods=["GET", "POST", "OPTIONS"],
-        allow_headers=["Content-Type", "Authorization"],
+        allow_methods=["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
     )
 
     application.include_router(auth.router)
